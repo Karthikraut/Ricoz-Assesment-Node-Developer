fabf4ff712f7138a400beea27f227a1848bf3c19
Karthikraut <karthikraut2@gmail.com>
2024-10-06 13:19:48 +0530

    Basic File Structure has been devloped

---
+++ connection/index.js
@@ -0,0 +1,13 @@
+const mongoose =require("mongoose");
+
+const connectToDB = async ()=>{
+    try{
+        const conn = await mongoose.connect(`mongodb://localhost:27017/Ricoz`)
+        console.log(`Mongodb connected: ${conn.connection.host}`)
+    }
+    catch(error){
+        console.log("Error in Connection: ",error);
+    }
+}
+
+export default connectToDB;
--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const express = require("express");
 const app = express();
+const dotenv = require('dotenv');
+const connectDB = require('./connection/index');
+
+dotenv.config();
+const PORT = process.env.PORT
+
+app.listen(PORT ,()=>{
+    console.log(`Hello ${process.env.PORT}`);
+    connectDB();
+    console.log(`Server has started AT ${PORT}`);
+})
